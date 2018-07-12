b99e2d7c51fd049d3b38807a0934c040eef03a51
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2018-07-12 13:54:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple script to scrape Dilbert comics and output a nice RSS feed for your local reader.
---
+++ dilbert.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python3
+# vim:ft=python:expandtab:sw=4:tw=140
+
+import sys
+import os
+import typing
+import re
+import time
+from pathlib import Path
+import sqlite3
+import datetime
+import http.client
+import urllib.request
+import lxml.html
+import lxml.etree
+import feedgenerator
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 7:
+    print("This script requires Python version 3.7")
+    sys.exit(1)
+
+
+# note: inclusive counting
+def _daterange(start_date: datetime.date, end_date: datetime.date) -> datetime.date:
+    for n in range(int((end_date - start_date).days) + 1):
+        yield start_date + datetime.timedelta(days=n)
+
+
+def _debug(*args):
+    print(args)
+
+
+class DilbertComic(object):
+    def __init__(self, row=None,
+                       comic_id=None, date=None, url=None, filename=None, title=None, updated=None, width=None, height=None):
+        if row:
+            self.id       = row['id']
+            self.url      = row['url']
+            self.filename = row['filename']
+            self.title    = row['title']
+            self.date     = datetime.date.fromisoformat(row['date'])
+            self.updated  = datetime.datetime.fromisoformat(row['updated'])
+            self.width    = row['width']
+            self.height   = row['height']
+        else:
+            self.id       = comic_id
+            self.date     = date
+            self.url      = url
+            self.filename = filename
+            self.title    = title
+            self.updated  = updated
+            self.width    = width
+            self.height   = height
+
+        # fix empty titles on sundays
+        if self.title == '':
+            self.title = "-"
+
+        _debug("Init comic {} for {}".format(self.url, self.date))
+
+    def __getitem__(self, key: str) -> [str, datetime.date]:
+        return self.__dict__[key]
+
+    def tag(self, baseurl: str='.') -> str:
+        w = ''
+        h = ''
+        if self.width:
+            w = 'width={}'.format(self.width)
+        if self.height:
+            h = 'height={}'.format(self.height)
+
+        url = '{}/{}'.format(baseurl, self.filename)
+        tag = '<img src="{}" alt="comic for {}" {} {}/>'.format(url, self.date, w, h)
+        return tag
+
+
+class Dilbert(object):
+    def __init__(self, basepath: os.PathLike, baseurl: str='.'):
+        self._basepath = Path(basepath)
+        self.db = self.open_db()
+        self.UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                + 'Chrome/35.0.1916.4 7 Safari/537.36'
+        self.baseurl = baseurl
+        self.feedname = "dilbert.rss"
+
+    def __del__(self):
+        self.db.close()
+
+    def open_db(self) -> sqlite3.Connection:
+        db = Path(self._basepath, "dilbert.sqlite3")
+        con = sqlite3.connect(db)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS `dilbert` (
+                id       INTEGER  PRIMARY KEY AUTOINCREMENT,
+                date     DATE     NOT NULL,
+                url      TEXT     NOT NULL,
+                title    TEXT,
+                filename TEXT     UNIQUE NOT NULL,
+                width    INT,
+                height   INT,
+                updated  DATETIME DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
+            );
+        """)
+        con.commit()
+        return con
+
+    def fetch_url(self, url: str) -> http.client.HTTPResponse:
+        req = urllib.request.Request(url, data=None, headers={'User-Agent': self.UA})
+        res = urllib.request.urlopen(req)
+        return res
+
+    # fetch most recent date present in database
+    def latest_date_in_db(self) -> datetime.date:
+        latest = None
+        for row in self.db.execute("select `date` from dilbert order by `date` desc limit 1"):
+            latest = datetime.date.fromisoformat(row['date'])
+        _debug("Latest date found is {}".format(str(latest)))
+        return latest
+
+    # fetch most recent date present in database
+    def comics(self, num: int=10) -> list():
+        _debug("Fetching {:d} latest comics from database".format(num))
+        comics = list()
+        for row in self.db.execute("select * from dilbert order by `date` desc limit {:d}".format(num)):
+            _debug("Found comic for {}".format(row['date']))
+            comics.append(DilbertComic(row=row))
+        return comics
+
+    def find_comic_by_date(self, date: datetime.date) -> DilbertComic:
+        _debug("Fetching comic for {}".format(date))
+        url = "http://dilbert.com/strip/{}".format(date.isoformat())
+        _debug("Fetching url `{}`".format(url))
+        res = self.fetch_url(url)
+        _debug("Status: {}".format(res.getcode()))
+        _debug("Headers: {}".format(res.info()))
+        body = res.read()
+        html = lxml.html.document_fromstring(body)
+        el = html.xpath("//img[@class and contains(concat(' ', normalize-space(@class), ' '), ' img-comic ')]")[0]
+        comic = DilbertComic(
+            date     = date,
+            url      = el.get('src'),
+            title    = re.sub(' - .*$', '', el.get('alt')),
+            filename = "{}.gif".format(date.isoformat()),
+            width    = el.get('width'),
+            height   = el.get('height'),
+        )
+        return comic
+
+    def download_comic(self, filename: str, url: str) -> None:
+        filename_full = 'cartoons/{}'.format(filename)
+        _debug("Downloading '{}' to '{}'".format(url, filename_full))
+        res = self.fetch_url(url)
+        _debug("Status: {}".format(res.getcode()))
+        _debug("Headers: {}".format(res.info()))
+        with open(filename_full, 'wb') as fd:
+            fd.write(res.read())
+        return
+
+    def write_comic_to_db(self, comic: DilbertComic) -> None:
+        cur = self.db.cursor()
+        _debug("Adding record for {} to database".format(comic.date))
+        cur.execute(
+            "insert into `dilbert` (date,url,title,filename,width,height) values (?,?,?,?,?,?)",
+            [comic[k] for k in ('date', 'url', 'title', 'filename', 'width', 'height')]
+        )
+        self.db.commit()
+        return
+
+    def update_comic_by_date(self, date: datetime.date) -> None:
+        comic = self.find_comic_by_date(date)
+        self.download_comic(comic.filename, comic.url)
+        self.write_comic_to_db(comic)
+
+    def update(self) -> None:
+        start = self.latest_date_in_db()
+        # default: 10 days ago
+        if not start:
+            start = datetime.date.today() - datetime.timedelta(days=10)
+
+        start = start + datetime.timedelta(days=1)
+
+        for date in _daterange(start, datetime.date.today()):
+            self.update_comic_by_date(date)
+            time.sleep(1.2)
+        return
+
+    def feed(self) -> feedgenerator.SyndicationFeed:
+        feed = feedgenerator.Atom1Feed(
+            title="De dagelijke Dilbert",
+            link="http://dilbert.com/",
+            feed_url = "{}/{}".format(self.baseurl, self.feedname),
+            description="Daily dilbert in a nice feed",
+            language="en",
+        )
+
+        for comic in self.comics(10):
+            _debug("Adding comic for {}".format(comic['date']))
+            feed.add_item(
+                title       = comic.title,
+                author_name = "Scott Adams",
+                link        = "http://dilbert.com/strip/{}".format(comic.date),
+                updateddate = comic.updated,
+                pubdate     = datetime.datetime.combine(comic.date, datetime.datetime.min.time()),
+                description = comic.tag(),
+            )
+        return feed
+
+    def rss(self) -> str:
+        return self.feed().writeString('UTF-8')
+
+    def write_rss(self, fd: typing.TextIO) -> None:
+        return self.feed().write(fd, 'UTF-8')
+
+
+if __name__ == "__main__":
+    dilbert = Dilbert(basepath=".", baseurl="http://localhost")
+    print(dilbert.update())
+    with open(dilbert.feedname, "w") as fd:
+        dilbert.write_rss(fd)
